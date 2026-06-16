8ec33961c15bf87213aa6453ec09857f01523337
kashish jain <jainkashish272@gmail.com>
2026-06-16 19:13:34 +0530

    functionality changed and updated some files

--- navbar.js
+++ navbar.js
@@ -1,22 +1,14 @@
 function nav() {
-    return `
-        <nav style="display:flex; font-size:20px; font-family:serif;">
-            <div style="margin-left:40px;">
-                <a href="" style="text-decoration:none;">Home</a>
-                <a href="" style="text-decoration:none; margin-left:28px;">Contact Us</a>
-                <a href="" style="text-decoration:none; margin-left:28px;">About Us</a>
-                <a href="" style="text-decoration:none; margin-left:28px;">Partner With Us</a>
-                <a href="" style="text-decoration:none; margin-left:28px;">Resources</a>
-            </div>
+    let result = "";
 
-            <div style="margin-left:430px;">
-                <button style="height:30px; width:70px;">Login</button>
-                <button style="height:30px; width:100px; margin-left:20px;">Sign Up</button>
-                <button style="height:30px; width:230px; margin-left:20px;">
-                    Get Started It's Free
-                </button>
-            </div>
-        </nav>
-        <hr>
-    `;
+    for(let i = 1; i <= 10; i++) {
+
+        result += `
+            <a href=" Home ${i}.html" style="text-decoration:none;">
+               <span style="color:black; margin-left:72px; font-size:20px; font-family:Bell MT ;" > Pages_${i}  </span>
+            </a>
+        `;
+    }	
+    return result;
 }
+
