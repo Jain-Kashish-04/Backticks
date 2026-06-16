1a2d6e153a6179a7aba00be06af21a96ca31f078
kashish jain <jainkashish272@gmail.com>
2026-06-16 19:43:17 +0530

    changed in CSS

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function nav() {
 
         result += `
             <a href=" Home ${i}.html" style="text-decoration:none;">
-               <span style="color:black; margin-left:72px; font-size:20px; font-family:Bell MT ;" > Pages_${i}  </span>
+               <span style="color:black; margin-left:67px; font-size:20px; font-family:Bell MT ;" > Pages_${i}  </span>
             </a>
         `;
     }	
